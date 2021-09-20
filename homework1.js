9d8f6fb20ac2d0f74c0e7f3414fae4e6270e91b9
Corpodean Andrei <andrei_corpodean@yahoo.ro>
2021-09-19 19:12:34 -0700

    homework 10

--- homework1.js
+++ homework1.js
@@ -70,6 +70,87 @@ var employees = [{
     salary: "668.98"
 }];
 
+const inventors = [{
+    first: "Albert",
+    last: "Einstein",
+    year: 1879,
+    passed: 1955
+}, {
+    first: "Isaac",
+    last: "Newton",
+    year: 1643,
+    passed: 1727
+}, {
+    first: "Galileo",
+    last: "Galilei",
+    year: 1564,
+    passed: 1642
+}, {
+    first: "Marie",
+    last: "Curie",
+    year: 1867,
+    passed: 1934
+}, {
+    first: "Johannes",
+    last: "Kepler",
+    year: 1571,
+    passed: 1630
+}, {
+    first: "Nicolaus",
+    last: "Copernicus",
+    year: 1473,
+    passed: 1543
+}, {
+    first: "Max",
+    last: "Planck",
+    year: 1858,
+    passed: 1947
+}, {
+    first: "Katherine",
+    last: "Blodgett",
+    year: 1898,
+    passed: 1979
+}, {
+    first: "Ada",
+    last: "Lovelace",
+    year: 1815,
+    passed: 1852
+}, {
+    first: "Sarah E.",
+    last: "Goode",
+    year: 1855,
+    passed: 1905
+}, {
+    first: "Lise",
+    last: "Meitner",
+    year: 1878,
+    passed: 1968
+}, {
+    first: "Hanna",
+    last: "Hammarström",
+    year: 1829,
+    passed: 1909
+}];
+
+
+const people = ["Bernhard, Sandra", "Bethea, Erin", "Becker, Carl", "Bentsen, Lloyd", "Beckett, Samuel", "Blake, William", "Berger, Ric", "Beddoes, Mick", "Beethoven, Ludwig", "Belloc, Hilaire", "Begin, Menachem", "Bellow, Saul", "Benchley, Robert", "Blair, Robert", "Benenson, Peter", "Benjamin, Walter", "Berlin, Irving", "Benn, Tony", "Benson, Leana", "Bent, Silas", "Berle, Milton", "Berry, Halle", "Biko, Steve", "Beck, Glenn", "Bergman, Ingmar", "Black, Elk", "Berio, Luciano", "Berne, Eric", "Berra, Yogi", "Berry, Wendell", "Bevan, Aneurin", "Ben-Gurion, David", "Bevel, Ken", "Biden, Joseph", "Bennington, Chester", "Bierce, Ambrose", "Billings, Josh", "Birrell, Augustine", "Blair, Tony", "Beecher, Henry", "Biondo, Frank"];
+
+// homework 6 
+
+const arrayToObject = (array, param) => {
+    const object = {};
+    array.forEach(obj => {
+        // object[obj[param]] = obj;
+        // object[obj['lastName']]=obj;
+        object[obj.lastName]=obj;
+    })
+    return object
+}
+
+console.log(arrayToObject(employees, 'lastName'))
+
+
+
 // homeWork 2 version 1
 
 function collectFirstName(employees) {
@@ -164,4 +245,22 @@ function splitEmployees(employees) {
 }
 
 console.info(splitEmployees(employees));
+
+// homework 9
+
+const filtered = inventors.filter( i=> i.year >= 1500 && i.year <1600 )
+console.info("filtered inventors 1500" ,  filtered);
+
+// homework 10
+
+// const FirstLastNames = inventors.map( n => {
+//     let f = n.first
+//     let l = n.last
+//     return [f,l]
+// });
+
+const FirstLastNames = inventors.map( n => [n.last,n.first]);
+    
+
+// homework 11
 
